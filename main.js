e336201ef88855b0b93a25bfe0bacd4da10e97a8
simplon <basilesimplon@gmail.com>
2021-02-24 10:55:03 +0100

    drumpadFinal

--- main.js
+++ main.js
@@ -20,6 +20,12 @@ window.addEventListener('keydown', function(e) {
     keys.forEach(key => {
         if (e.keyCode == key.getAttribute('data-key')) {
             key.classList.add('playing')
+
+            key.addEventListener('transitionend', function(e) {
+
+                        key.classList.remove('playing')
+
+            })
         }
     })
 
@@ -27,14 +33,69 @@ window.addEventListener('keydown', function(e) {
 
 })
 
-window.addEventListener('keyup', function(e) {
 
-    keys.forEach(key => {
-        if (e.keyCode == key.getAttribute('data-key')) {
-            key.classList.remove('playing')
-        }
-    })
-})
+
+
+
+//DECLARATION
+
+
+
+async function beatBox() {
+    function simulateKey(key) {
+        var boxkey = new Event('keydown',{
+            bubbles : true
+        });
+        boxkey.keyCode = key
+        window.dispatchEvent(boxkey)
+    }
+    function playBeat(time,key){
+        return new Promise((resolve,reject) => {
+            setTimeout(() => {
+            resolve(simulateKey(key));
+            },time)
+        })
+        
+    }
+
+    await playBeat(50, 90)
+    await playBeat(400, 83)
+    await playBeat(50, 67)
+    await playBeat(420, 83)
+    await playBeat(420, 83)
+    await playBeat(420, 83)
+   
+    await playBeat(50, 90)
+    await playBeat(420, 83)
+    await playBeat(50, 67)
+    await playBeat(420, 83)
+    await playBeat(420, 83)
+    await playBeat(420, 83)
+    
+    await playBeat(50, 90)
+    await playBeat(420, 83)
+    await playBeat(50, 67)
+    await playBeat(420, 83)
+    await playBeat(420, 83)
+    await playBeat(420, 83)
+    
+    await playBeat(50, 90)
+    await playBeat(420, 83)
+    await playBeat(50, 67)
+    await playBeat(420, 83)
+    await playBeat(420, 83)
+    await playBeat(420, 83)
+    
+    await playBeat(50, 90)
+    await playBeat(420, 83)
+    await playBeat(50, 67)
+    await playBeat(420, 83)
+    await playBeat(420, 83)
+    await playBeat(420, 83)
+    
+}
+
+
 
 
 
